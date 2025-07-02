b6d01f9ed2dfc0b626de4559915710a5f7752938
Aswin-art <aswinarung2@gmail.com>
2025-07-02 12:07:26 +0700

    feat: add file route & middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,34 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { authMiddleware } from "./middleware/auth-middleware";
 
+const redirectMap: { [key: string]: string } = {
+  "/admin/dashboard": "/admin/dashboard/statistic/umkm-analytic",
+  "/admin/dashboard/statistic": "/admin/dashboard/statistic/umkm-analytic",
+  "/admin/dashboard/umkm-data": "/admin/dashboard/umkm-data/umkm-list",
+  "/admin/dashboard/fund-program": "/admin/dashboard/fund-program/program-list",
+
+  "/umkm/dashboard": "/umkm/dashboard/statistic/umkm-analytic",
+  "/umkm/dashboard/statistic": "/umkm/dashboard/statistic/umkm-analytic",
+  "/umkm/dashboard/umkm-data": "/umkm/dashboard/umkm-data/umkm-list",
+  "/umkm/dashboard/fund-program": "/umkm/dashboard/fund-program/program-list",
+};
+
 export function middleware(req: NextRequest) {
-  // authMiddleware
-  // const response = authMiddleware(req)
+  const { pathname } = req.nextUrl;
+  // const response = authMiddleware(req);
   // if (response) {
-  //   return response
+  //   return response;
   // }
+
+  if (pathname in redirectMap) {
+    const url = req.nextUrl.clone();
+    url.pathname = redirectMap[pathname];
+    return NextResponse.redirect(url);
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/auth/login"],
+  matcher: ["/admin/:path*", "/auth/:path*"],
 };
--- src/navigation/sidebar/sidebar-items.ts
+++ src/navigation/sidebar/sidebar-items.ts
@@ -52,42 +52,154 @@ export interface NavGroup {
   items: NavMainItem[];
 }
 
-export const sidebarItems: NavGroup[] = [
+export const adminSidebarItems: NavGroup[] = [
   {
     id: 1,
     label: "Dashboards",
     items: [
       {
         title: "Analisa Statistik",
-        url: "/dashboard",
+        url: "/admin/dashboard/statistic",
         icon: Home,
         subItems: [
-          { title: "Analisa UMKM", url: "/dashboard/default", icon: ChartPie },
-          { title: "Sebaran Wilayah", url: "/dashboard", icon: Grid2X2 },
-          { title: "Pertumbuhan", url: "/dashboard/analytics", icon: ChartLine },
-          // { title: "eCommerce", url: "/dashboard/e-commerce", icon: ShoppingBag, comingSoon: true },
-          // { title: "Academy", url: "/dashboard/academy", icon: BookA, comingSoon: true },
-          // { title: "Logistics", url: "/dashboard/logistics", icon: Forklift, comingSoon: true },
+          { title: "Analisa UMKM", url: "/admin/dashboard/statistic/umkm-analytic", icon: ChartPie },
+          { title: "Sebaran Wilayah", url: "/admin/dashboard/statistic/umkm-regional", icon: Grid2X2 },
+          { title: "Pertumbuhan", url: "/admin/dashboard/statistic/umkm-growth", icon: ChartLine },
+          // { title: "eCommerce", url: "/admin/dashboard/e-commerce", icon: ShoppingBag, comingSoon: true },
+          // { title: "Academy", url: "/admin/dashboard/academy", icon: BookA, comingSoon: true },
+          // { title: "Logistics", url: "/admin/dashboard/logistics", icon: Forklift, comingSoon: true },
         ],
       },
       {
         title: "Data UMKM",
-        url: "/dashboard/umkm",
+        url: "/admin/dashboard/umkm-data",
         icon: ShoppingBag,
         subItems: [
-          { title: "Daftar UMKM", url: "/dashboard/umkm", icon: List },
-          { title: "Verifikasi UMKM", url: "/dashboard/umkm", icon: CheckCheck },
-          { title: "Aktivitas UMKM", url: "/dashboard", icon: Activity },
+          { title: "Daftar UMKM", url: "/admin/dashboard/umkm-data/umkm-list", icon: List },
+          { title: "Verifikasi UMKM", url: "/admin/dashboard/umkm-data/umkm-verification", icon: CheckCheck },
+          { title: "Aktivitas UMKM", url: "/admin/dashboard/umkm-data/umkm-activity", icon: Activity },
         ],
       },
       {
         title: "Program Bantuan",
-        url: "/dashboard/program",
+        url: "/admin/dashboard/fund-program",
         icon: BookA,
         subItems: [
-          { title: "Daftar Program", url: "/dashboard/program", icon: CalendarCheck },
-          { title: "UMKM Penerima", url: "/dashboard", icon: UserCheck },
-          { title: "Riwayat Bantuan", url: "/dashboard/analytics", icon: History },
+          { title: "Daftar Program", url: "/admin/dashboard/fund-program/program-list", icon: CalendarCheck },
+          { title: "UMKM Penerima", url: "/admin/dashboard/fund-program/umkm-selected-list", icon: UserCheck },
+          { title: "Riwayat Bantuan", url: "/admin/dashboard/fund-program/fund-history", icon: History },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    label: "Users",
+    items: [
+      {
+        title: "Kelola Admin",
+        url: "/auth",
+        icon: Users,
+        subItems: [
+          { title: "Daftar Pengguna", url: "/auth/v1/login", newTab: true, icon: User },
+          { title: "Verifikasi Pengguna", url: "/auth/v1/register", newTab: true, icon: UserPlus },
+          { title: "Hak Akses", url: "/auth/v1/register", newTab: true, icon: Lock },
+        ],
+      },
+      // {
+      //   title: "Email",
+      //   url: "/mail",
+      //   icon: Mail,
+      //   comingSoon: true,
+      // },
+      // {
+      //   title: "Chat",
+      //   url: "/chat",
+      //   icon: MessageSquare,
+      //   comingSoon: true,
+      // },
+      // {
+      //   title: "Calendar",
+      //   url: "/calendar",
+      //   icon: Calendar,
+      //   comingSoon: true,
+      // },
+      // {
+      //   title: "Kanban",
+      //   url: "/kanban",
+      //   icon: Kanban,
+      //   comingSoon: true,
+      // },
+      // {
+      //   title: "Invoice",
+      //   url: "/invoice",
+      //   icon: ReceiptText,
+      //   comingSoon: true,
+      // },
+      // {
+      //   title: "Users",
+      //   url: "/users",
+      //   icon: Users,
+      //   comingSoon: true,
+      // },
+      // {
+      //   title: "Roles",
+      //   url: "/roles",
+      //   icon: Lock,
+      //   comingSoon: true,
+      // },
+    ],
+  },
+  {
+    id: 3,
+    label: "Others",
+    items: [
+      {
+        title: "Pengaturan",
+        url: "/others",
+        icon: Cog,
+        comingSoon: true,
+      },
+    ],
+  },
+];
+
+export const umkmSidebarItems: NavGroup[] = [
+  {
+    id: 1,
+    label: "Dashboards",
+    items: [
+      {
+        title: "Analisa Statistik",
+        url: "/umkm/dashboard/statistic",
+        icon: Home,
+        subItems: [
+          { title: "Analisa UMKM", url: "/umkm/dashboard/statistic/umkm-analytic", icon: ChartPie },
+          { title: "Sebaran Wilayah", url: "/umkm/dashboard/statistic/umkm-regional", icon: Grid2X2 },
+          { title: "Pertumbuhan", url: "/umkm/dashboard/statistic/umkm-growth", icon: ChartLine },
+          // { title: "eCommerce", url: "/umkm/dashboard/e-commerce", icon: ShoppingBag, comingSoon: true },
+          // { title: "Academy", url: "/umkm/dashboard/academy", icon: BookA, comingSoon: true },
+          // { title: "Logistics", url: "/umkm/dashboard/logistics", icon: Forklift, comingSoon: true },
+        ],
+      },
+      {
+        title: "Data UMKM",
+        url: "/umkm/dashboard/umkm-data",
+        icon: ShoppingBag,
+        subItems: [
+          { title: "Daftar UMKM", url: "/umkm/dashboard/umkm-data/umkm-list", icon: List },
+          { title: "Verifikasi UMKM", url: "/umkm/dashboard/umkm-data/umkm-verification", icon: CheckCheck },
+          { title: "Aktivitas UMKM", url: "/umkm/dashboard/umkm-data/umkm-activity", icon: Activity },
+        ],
+      },
+      {
+        title: "Program Bantuan",
+        url: "/umkm/dashboard/fund-program",
+        icon: BookA,
+        subItems: [
+          { title: "Daftar Program", url: "/umkm/dashboard/fund-program/program-list", icon: CalendarCheck },
+          { title: "UMKM Penerima", url: "/umkm/dashboard/fund-program/umkm-selected-list", icon: UserCheck },
+          { title: "Riwayat Bantuan", url: "/umkm/dashboard/fund-program/fund-history", icon: History },
         ],
       },
     ],
